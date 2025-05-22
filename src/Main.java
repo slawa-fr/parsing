39e5a1fff92c5d68ca3654604f445dee5f302a0d
slawa-fr <slawa-fr@mail.ru>
2025-05-22 20:28:09 +0300

    03-setting git

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
+
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
